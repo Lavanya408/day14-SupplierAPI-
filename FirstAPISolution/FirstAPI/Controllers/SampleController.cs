075de9d906d834ccef8568e5d57325d01d74e4c9
lavanya.bv <lavanya.bv@INBL01LAP7023>
2023-09-12 22:28:15 +0530

    Commit

---
+++ FirstAPISolution/FirstAPI/Controllers/SampleController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace FirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        //[HttpGet]
+        //public string Get() 
+        //{
+        // return "Hello";
+        //}
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(new string[] { "Hello", "Hi", "Welcome", "Bye" });
+        }
+        [HttpGet("GetById")]
+        public ActionResult GetAnother(int idx)
+        {
+            return Ok(new string[] { "Hello", "Hi", "Welcome", "Bye" }[idx]);
+        }
+        [HttpPost]
+        public ActionResult Lavanya(string name)
+        {
+            if (name.Length > 3)
+                return Ok(name);
+            return BadRequest();
+        }
+        [HttpPut]
+        public ActionResult Hindu(student student)
+        {
+            if (student.Id == 101)
+            {
+                student.Name = "laav";
+                return Ok(student);
+            }
+
+            return BadRequest();
+        }
+        [HttpDelete]
+        public ActionResult siva(int id)
+        {
+            if (id == 101)
+                return Ok();
+            return Ok();
+        }
+
+        public class student
+        {
+
+            public int Id { get; set; }
+
+
+            public string Name { get; set; }
+        }
+    }
+
+
+
+}
---
+++ ProductManagamentAPISolution/ProductManagamentAPI/Models/Supplier.cs
@@ -0,0 +1,16 @@
+﻿namespace SupplierAPI.Models
+{
+    public class Supplier
+    {
+
+        public int Id { get; set; }
+        public String Name { get; set; }
+
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+
+        public List<Product>? Products { get; set; }
+
+    }
+}
